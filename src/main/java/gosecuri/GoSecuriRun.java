6d8431890d6f36f934e38dc7b4213d47b60713b8
CalD2 <95912602+CalD2@users.noreply.github.com>
2022-04-19 15:14:15 +0200

    refacto

---
+++ src/main/java/gosecuri/GoSecuriRun.java
@@ -0,0 +1,38 @@
+package gosecuri;
+
+import java.io.File;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+import utils.Utils;
+
+public class GoSecuriRun
+{
+	/**
+	 * Déclanche la création de page html à partir de fichiers fournis
+	 * 
+	 * @param directory        répertoire des infos général
+	 * @param directoryIdCards répertoire des cartes d'identités
+	 * @param directoryStaffs  répertoire des fiches du staffs
+	 * @throws Exception
+	 */
+	public void run(String directory, String directoryIdCards, String directoryStaffs) throws Exception
+	{
+
+		// init du matériel de la factory
+		StaffFactory.factory.buildsAll(new File(directory + "liste.txt"));
+
+		// On construit la liste des agents
+		List<String> lines = FileUtils.readLines(new File(directory + "staff.txt"), Utils.ENCODING);
+		List<Staff> staffs = StaffFactory.builds(lines, directoryStaffs, directoryIdCards);
+
+		// On tri les agents par ordre alphabétique
+		staffs.sort(Comparator.comparing(Staff::getId));
+
+		// On génère les pages html
+		HtmlFactory.buildIndex(staffs);
+		HtmlFactory.build(staffs);
+	}
+}

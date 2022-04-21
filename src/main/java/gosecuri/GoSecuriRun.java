651b8707f069a09819ac99a29752142b0c069b18
CalD2 <95912602+CalD2@users.noreply.github.com>
2022-04-21 14:01:47 +0200

    log

--- src/main/java/gosecuri/GoSecuriRun.java
+++ src/main/java/gosecuri/GoSecuriRun.java
@@ -19,19 +19,24 @@ public class GoSecuriRun
 	 */
 	public void run(String directory, String directoryIdCards, String directoryStaffs) throws Exception
 	{
-
+		System.out.println("---------------------INIT---------------------");
 		// init du matériel de la factory
 		StaffFactory.factory.buildsAll(new File(directory + Utils.LISTE_TXT));
+		System.out.println("---MATERIEL : OK");
 
 		// On construit la liste des agents
 		List<String> lines = FileUtils.readLines(new File(directory + Utils.STAFF_TXT), Utils.ENCODING);
 		List<Staff> staffs = StaffFactory.builds(lines, directoryStaffs, directoryIdCards);
+		System.out.println("---STAFFS : OK");
 
 		// On tri les agents par ordre alphabétique de leur ID
 		Utils.sortStaff(staffs);
 
 		// On génère les pages html
 		HtmlFactory.buildIndex(staffs);
+		System.out.println("---INDEX HTML : OK");
 		HtmlFactory.build(staffs);
+		System.out.println("---STAFF HTML : OK");
+		System.out.println("---------------------END---------------------");
 	}
 }
